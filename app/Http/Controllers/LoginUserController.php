e01208ef4ba52e649c068f772636f29684388cdf
Hartoley <tolanijimoh1@gmail.com>
2025-01-06 21:20:31 +0000

    Login double route working

--- app/Http/Controllers/LoginUserController.php
+++ app/Http/Controllers/LoginUserController.php
@@ -2,52 +2,67 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\LoginUser;
+use App\Models\LoginUser; // Ensure this model has the necessary fields
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class LoginUserController extends Controller
 {
-    //
-    public function LoginUser(Request $request){
-        $email =$request->input('email');
-        $surname =$request->input('surname');
-        $first_name =$request->input('first_name');
-        $password =$request->input('password');
-
-
-
-        LoginUser::create([
-            'email'=> $email,
-            'surname'=> $surname,
-            'first_name'=> $first_name,
-            'password'=> $password,
+    // Method to handle user registration
+    public function LoginUser(Request $request)
+    {
+        // Validate the incoming request
+        $request->validate([
+            'email' => 'required|email|unique:login_users,email', // Ensure email is unique
+            'surname' => 'required|string',
+            'first_name' => 'required|string',
+            'password' => 'required|string|min:6', // Ensure password is at least 6 characters
         ]);
 
-        return response()->json((["message"=> "Reegistration successful", 200]));
+        // Create a new user
+        LoginUser::create([
+            'email' => $request->input('email'),
+            'surname' => $request->input('surname'),
+            'first_name' => $request->input('first_name'),
+            'password' => Hash::make($request->input('password')), // Hash the password
+            'role' => 'user', // Set a default role or handle it as needed
+        ]);
+
+        return response()->json(["message" => "Registration successful"], 201);
     }
 
+    // Method to handle user sign-in
     public function signIn(Request $request)
     {
+        // Validate the incoming request
         $request->validate([
             'email' => 'required|email',
             'password' => 'required|string',
         ]);
-    
+
+        // Retrieve the credentials from the request
         $credentials = $request->only('email', 'password');
-    
+
+        // Attempt to authenticate the user with the provided credentials
         if (Auth::attempt($credentials)) {
+            // If authentication is successful, retrieve the authenticated user
             $user = Auth::user();
+
+            // Return a JSON response with user information, including the role
             return response()->json([
                 'success' => true,
                 'message' => 'Sign-in successful',
-                'user' => $user,
+                'user' => [
+                    'id' => $user->id,
+                    'name' => $user->name, // Assuming you have these fields
+                    'email' => $user->email,
+                    'role' => $user->role, // Include the role
+                ],
             ], 200);
         }
-    
+
+        // If authentication fails, return an error response
         return response()->json(['success' => false, 'message' => 'Invalid credentials'], 401);
     }
-
-    
 }
--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -1,89 +1,176 @@
 <template>
     <div>
-      <Header />
-      <div class="w-full h-[100vh] relative flex flex-col items-center justify-center">
-        <div class="h-full w-full absolute">
-          <img class="w-full h-full" :src="image.src" :alt="image.alt" />
-        </div>
-        <div class="w-[70%] z-10 flex items-center mt-10 h-[80%]">
-          <img class="w-[50%] h-full rounded-bl-md rounded-tl-md" :src="images.src" :alt="image.alt" />
-          <div class="w-[50%] h-full rounded-br-md rounded-tr-md bg-[rgba(59,83,145,0.96)] flex flex-col">
-            <div class="w-[40%] h-[15%] ml-[60%] flex items-center justify-center">
-              <button class="w-[45%] h-[60%] rounded-tl-[20px] rounded-bl-[20px] text-white bg-[rgb(46,61,84)] hover:bg-[rgb(169,196,219)] transition duration-100">Sign up</button>
-              <button class="w-[45%] h-[60%] rounded-tr-[20px] rounded-br-[20px] bg-[rgb(218,211,167)] hover:bg-[rgb(200,188,113)] transition duration-100">Sign in</button>
+        <Header />
+        <div
+            class="w-full h-[100vh] relative flex flex-col items-center justify-center"
+        >
+            <div class="h-full w-full absolute">
+                <img class="w-full h-full" :src="image.src" :alt="image.alt" />
             </div>
-            <div class="grid w-full h-auto place-items-center">
-              <img class="w-[5vw]" src="https://i.pinimg.com/474x/c2/45/31/c245311698a8e4801bc402c592714e92.jpg" alt="" />
-            </div>
-            <div class="w-[100%] flex flex-col items-center justify-center text-white gap-10 h-[80%]">
-              <input type="email" v-model="email" class="w-[70%] border-b-white border-l-[rgba(59,83,145,0.96)] border-r-[rgba(59,83,145,0.96)] border-t-[rgba(59,83,145,0.96)] placeholder-white outline-none bg-[rgba(59,83,145,0.96)] text-white" placeholder="EMAIL" />
-              <div class="relative w-[70%]">
-                <input
-                  :type="showPassword ? 'text' : 'password'"
-                  v-model="password"
-                  class="w-full border-b-white border-l-[rgba(59,83,145,0.96)] border-r-[rgba(59,83,145,0.96)] border-t-[rgba(59,83,145,0.96)] placeholder-white outline-none bg-[rgba(59,83,145,0.96)] text-white"
-                  placeholder="PASSWORD"
+            <div class="w-[70%] z-10 flex items-center mt-10 h-[80%]">
+                <img
+                    class="w-[50%] h-full rounded-bl-md rounded-tl-md"
+                    :src="images.src"
+                    :alt="image.alt"
                 />
-                <button @click="togglePassword" class="absolute right-0 top-1/2 transform -translate-y-1/2 pr-3 text-white">
-                  <span v-if="showPassword">Hide</span>
-                  <span v-else>Show</span>
-                </button>
-              </div>
-              <button @click="signup" class="w-[60%] h-[10%] hover:bg-[rgb(169,196,219)] transition duration-100 rounded-[25px] rounded-bl-[20px] text-white bg-[rgb(46,61,84)]">Sign in</button>
+                <div
+                    class="w-[50%] h-full rounded-br-md rounded-tr-md bg-[rgba(59,83,145,0.96)] flex flex-col"
+                >
+                    <div
+                        class="w-[40%] h-[15%] ml-[60%] flex items-center justify-center"
+                    >
+                        <button
+                            class="w-[45%] h-[60%] rounded-tl-[20px] rounded-bl-[20px] text-white bg-[rgb(46,61,84)] hover:bg-[rgb(169,196,219)] transition duration-100"
+                        >
+                            Sign up
+                        </button>
+                        <button
+                            class="w-[45%] h-[60%] rounded-tr-[20px] rounded-br-[20px] bg-[rgb(218,211,167)] hover:bg-[rgb(200,188,113)] transition duration-100"
+                        >
+                            Sign in
+                        </button>
+                    </div>
+                    <div class="grid w-full h-auto place-items-center">
+                        <img
+                            class="w-[5vw]"
+                            src="https://i.pinimg.com/474x/c2/45/31/c245311698a8e4801bc402c592714e92.jpg"
+                            alt=""
+                        />
+                    </div>
+                    <div
+                        class="w-[100%] flex flex-col items-center justify-center text-white gap-10 h-[80%]"
+                    >
+                        <input
+                            type="email"
+                            v-model="email"
+                            class="w-[70%] border-b-white border-l-[rgba(59,83,145,0.96)] border-r-[rgba(59,83,145,0.96)] border-t-[rgba(59,83,145,0.96)] placeholder-white outline-none bg-[rgba(59,83,145,0.96)] text-white"
+                            placeholder="EMAIL"
+                        />
+                        <div class="relative w-[70%]">
+                            <input
+                                :type="showPassword ? 'text' : 'password'"
+                                v-model="password"
+                                class="w-full border-b-white border-l-[rgba(59,83,145,0.96)] border-r-[rgba(59,83,145,0.96)] border-t-[rgba(59,83,145,0.96)] placeholder-white outline-none bg-[rgba(59,83,145,0.96)] text-white"
+                                placeholder="PASSWORD"
+                            />
+                            <button
+                                @click="togglePassword"
+                                class="absolute right-0 top-1/2 transform -translate-y-1/2 pr-3 text-gray-700"
+                            >
+                                <span v-if="showPassword">Hide</span>
+                                <span v-else>Show</span>
+                            </button>
+                        </div>
+                        <div
+                            class="w-[60%] h-[10%] flex items-center justify-center"
+                        >
+                            <button
+                                @click="signin"
+                                :disabled="loading"
+                                class="w-full h-full hover:bg-[rgb(169,196,219)] transition duration-100 rounded-[25px] rounded-bl-[20px] text-white bg-[rgb(46,61,84)]"
+                            >
+                                <span v-if="!loading">Sign in</span>
+                                <span v-else>
+                                    <i class="fas fa-spinner fa-spin"></i>
+                                    Loading...
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </template>
-  
-  <script>
-  import Header from './Header.vue';
-  import axios from 'axios';
-  import "@fortawesome/fontawesome-free/css/all.min.css";
-  
-  export default {
+</template>
+
+<script>
+import Header from "./Header.vue";
+import axios from "axios";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+export default {
     components: {
-      Header,
+        Header,
     },
     data() {
-      return {
-        email: '',
-        password: '',
-        showPassword: false, 
-        image: { src: 'https://i.pinimg.com/474x/2f/cb/76/2fcb76ac0eabfa6c6f5d3bd6d90b3678.jpg', alt: 'Picture 1' },
-        images: { src: 'https://i.pinimg.com/474x/e3/1f/4a/e31f4a55e90e9752df6d84eca09b4d52.jpg', alt: 'Picture 2' },
-      };
+        return {
+            email: "",
+            password: "",
+            showPassword: false,
+            loading: false,
+            image: {
+                src: "https://i.pinimg.com/474x/2f/cb/76/2fcb76ac0eabfa6c6f5d3bd6d90b3678.jpg",
+                alt: "Picture 1",
+            },
+            images: {
+                src: "https://i.pinimg.com/474x/e3/1f/4a/e31f4a55e90e9752df6d84eca09b4d52.jpg",
+                alt: "Picture 2",
+            },
+        };
     },
     methods: {
-      togglePassword() {
-        this.showPassword = !this.showPassword; 
-      },
-      signup() {
-        let data = {
-          email: this.email,
-          password: this.password,
-        };
-  
-        axios.post(route("login", data))
-          .then(response => {
-            alert('Sign in successfully');
-            console.log('Sign in successfully:', response);
-            this.email = '';
-            this.password = '';
-            
-            setTimeout(() => {
-                window.location.href = '/keenaTravel';
-                }, 1000); 
-            })
-          .catch(error => {
-            console.error('There was an error submitting the form:', error);
-          });
-      }
+        togglePassword() {
+            this.showPassword = !this.showPassword;
+        },
+
+        signin() {
+            this.loading = true;
+            let data = {
+                email: this.email,
+                password: this.password,
+            };
+
+            axios
+                .post(route("users.signin"), data, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json",
+                    },
+                })
+                .then((response) => {
+                    console.log("Sign in successfully:", response.status);
+                    console.log("Sign in message:", response.data.message);
+                    this.email = "";
+                    this.password = "";
+                    this.loading = false;
+
+                    if (response.data) {
+                        console.log(response.data);
+                        console.log(response.data.user.role);
+                    } else {
+                        console.log("No response data");
+                    }
+
+                    if (response.data.user && response.data.user.role) {
+                        if (response.data.user.role === "User") {
+                            setTimeout(() => {
+                                console.log("Yes I am a user");
+
+                                // window.location.href = "/keenaTravel";
+                            }, 1000);
+                        } else {
+                            console.log(
+                                "Unauthorized access: Role is not User"
+                            );
+                        }
+                    } else {
+                        console.log("User data not found in response");
+                    }
+                })
+                .catch((error) => {
+                    console.error(
+                        "There was an error submitting the form:",
+                        error
+                    );
+                    this.loading = false;
+                    if (error.response) {
+                        console.error(
+                            "Error response data:",
+                            error.response.data
+                        );
+                    }
+                });
+        },
     },
-    mounted() {
-      this.fetching();
-    },
-  };
-  </script>
-  
+};
+</script>
